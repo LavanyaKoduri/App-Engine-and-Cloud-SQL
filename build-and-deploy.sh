b89edfc648e38c45b22237667b0d4e562ccbc474
Lavanya Koduri <lavanyakoduri999@gmail.com>
2023-04-25 14:14:46 +0530

    CloudSQL and App engine

---
+++ build-and-deploy.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+export PROJECT_ID=docker-demo-384011
+export REGION=us-central1
+export CONNECTION_NAME=docker-demo-384011:us-central1:poll-database
+
+gcloud builds submit \
+  --tag gcr.io/$PROJECT_ID/poll \
+  --project $PROJECT_ID
+
+gcloud app deploy poll \
+  --image gcr.io/$PROJECT_ID/poll \
+  --platform managed \
+  --region $REGION \
+  --allow-unauthenticated \
+  --add-cloudsql-instances $CONNECTION_NAME \
+  --project $PROJECT_ID
